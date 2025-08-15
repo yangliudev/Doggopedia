3980fa43fb84794e5be009273132db977773fac6
Yang Liu <ylyangliu1@gmail.com>
2025-08-14 23:36:11 -0700

    edit package name and prepare keystore files

--- android/app/src/main/java/com/doggopedia/MainActivity.kt
+++ android/app/src/main/java/com/doggopedia/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.yangliudev.doggopedia
+package com.doggopedia
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/doggopedia/MainApplication.kt
+++ android/app/src/main/java/com/doggopedia/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.yangliudev.doggopedia
+package com.doggopedia
 
 import android.app.Application
 import android.content.res.Configuration
